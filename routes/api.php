3897d6e10d4da7c9e99fb3e78872c948427d8e6f
Brian Lee <brian@Brians-MacBook-Pro.local>
2022-07-06 01:13:55 +0800

    time adj

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,9 @@ use App\Http\Controllers\VendingMachineTransactionController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::post('/v1/vending-machine-transaction', [
     VendingMachineTransactionController::class, 'create'
