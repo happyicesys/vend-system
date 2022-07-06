2bd0788f056bc04c42abfc0ebce8b1bf413a8b94
Brian Lee <brian@Brians-MacBook-Pro.local>
2022-07-06 20:51:13 +0800

    remove thottle limit

--- app/Models/VendingMachineTransaction.php
+++ app/Models/VendingMachineTransaction.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,4 +17,12 @@ class VendingMachineTransaction extends Model
     protected $casts = [
         'original' => 'json'
     ];
+
+    protected function original(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value,
+            set: fn ($value) => stripcslashes($value),
+        );
+    }
 }
